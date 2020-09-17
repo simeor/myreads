4d94331cd52ec2ffd14d0998d33a15e49be97d56
Dorothy Yau <dkyyau@gmail.com>
2020-09-17 21:39:59 +0100

    Changed book controller and seeded categories

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,42 +7,4 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-
-  def new
-    @book = Book.new
-  end
-
-  def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to root_path
-    else
-      render 'new'
-    end
-  end
-
- def edit
-  @book = Book.find(params[:id])
- end
-
-
- def update
-  @book = Book.find(params[:id])
-  @book.update(book_params)
-  redirect_to root_path
- end
-
- def destroy
-  @book = Book.find(params[:id])
-  @book.destroy
-  edirect_to root_path
- end
-
-private
-
-def book_params
-  params.require(:book).permit(:title, :author, :summary, :year, :category_id)
-end
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :books, only: [:index, :show, :new, :create]
+  get 'dashboard', to: 'pages#dashboard'
+  resources :books, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
