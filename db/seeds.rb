8a85b216a1a7363f2e37a7030982c9667133bdfd
simeo russo <simeo94@hotmail.com>
2020-09-23 19:49:38 +0100

    created seed that creates 100 books for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.destroy_all
+require 'json'
+require 'open-uri'
 
-Category.create(name: "Science Fiction")
-Category.create(name: "Biography")
-Category.create(name: "Memoir")
-Category.create(name: "Business")
-Category.create(name: "Poetry")
-Category.create(name: "Romance")
-Category.create(name: "Horror")
-Category.create(name: "Thriller")
-Category.create(name: "Classics")
-Category.create(name: "Young Adult")
-Category.create(name: "Comedy")
-Category.create(name: "Non-fiction")
+# Category.destroy_all
+Book.destroy_all
+
+# Category.create(name: "Science Fiction")
+# Category.create(name: "Biography")
+# Category.create(name: "Memoir")
+# Category.create(name: "Business")
+# Category.create(name: "Poetry")
+# Category.create(name: "Romance")
+# Category.create(name: "Horror")
+# Category.create(name: "Thriller")
+# Category.create(name: "Classics")
+# Category.create(name: "Young Adult")
+# Category.create(name: "Comedy")
+# Category.create(name: "Non-fiction")
+
+
+
+url = "https://gist.githubusercontent.com/simeor/7db5681f7102151547a2589cdabf21d7/raw/2e379e05cad5bf828ee1e5910a0263985d7753f4/books.json"
+books_serialized = open(url).read
+books_list = JSON.parse(books_serialized)
+
+
+books_list.each do |book|
+ Book.create(title: book['title'], author: book['author'], summary:"", year: book['year'],  category_id: 1 )
+ p "created #{book['title']}"
+end
